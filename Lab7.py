9dd9daeb51ff8790242f317efb03ab1cc4fbb84b
Yerbossyn <erbosynerkinuly@gmail.com>
2024-04-17 13:00:57 +0500

    изменение в файлах

---
+++ Lab7.py
@@ -0,0 +1,80 @@
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return None
+    else:
+        return x / y
+
+
+def main():
+    x = float(input())
+    y = float(input())
+
+    result_add = add(x, y)
+    print("+:", result_add)
+
+    result_subtract = subtract(x, y)
+    print("-:", result_subtract)
+
+    result_multiply = multiply(x, y)
+    print("*:", result_multiply)
+
+    result_divide = divide(x, y)
+    print("/:", result_divide)
+
+
+if __name__ == "__main__":
+    main()
+
+
+def add(x, y):
+    return x + y
+
+
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    if y == 0:
+        return None
+    else:
+        return x / y
+
+
+def main():
+    x = float(input())
+    y = float(input())
+
+    result_add = add(x, y)
+    print("+:", result_add)
+
+    result_subtract = subtract(x, y)
+    print("-:", result_subtract)
+
+    result_multiply = multiply(x, y)
+    print("*:", result_multiply)
+
+    result_divide = divide(x, y)
+    print("/:", result_divide)
+
+
+if __name__ == "__main__":
+    main()
+
+
